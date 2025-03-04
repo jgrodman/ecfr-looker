58f0bbd3f97e0bbd8f9de817e486e8c2e71d6b9f
Jesse Grodman <jesse@triplewhale.com>
2025-03-04 08:43:47 +0200

    titles

--- src/pages/api/init.ts
+++ src/pages/api/init.ts
@@ -8,7 +8,8 @@ interface AgencyResponse {
 export default async function initialize(req: NextApiRequest, res: NextApiResponse) {
   try {
     await initializeDatabase();
-    await agencies();
+    await fetchAgencies();
+    await fetchTitles();
     res.status(200).json({ message: 'Database initialized successfully' });
   } catch (error) {
     console.error('Error initializing database:', error);
@@ -16,7 +17,7 @@ export default async function initialize(req: NextApiRequest, res: NextApiRespon
   }
 }
 
-async function agencies() {
+async function fetchAgencies() {
   const response = await fetch('https://www.ecfr.gov/api/admin/v1/agencies.json', {
     headers: {
       accept: 'application/json',
@@ -30,4 +31,33 @@ async function agencies() {
   const data: AgencyResponse = await response.json();
 
   await saveAgencies(data.agencies);
+}
+
+interface TitleResponse {
+  titles: Title[];
+}
+
+interface Title {
+  number: number;
+  name: string;
+  latest_amended_on: string;
+  latest_issue_date: string;
+  up_to_date_as_of: string;
+  reserved: boolean;
+}
+
+async function fetchTitles() {
+  const response = await fetch('https://www.ecfr.gov/api/versioner/v1/titles.json', {
+    headers: {
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data: TitleResponse = await response.json();
+
+  console.log(data);
 }
